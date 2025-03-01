bd72ba6020e3efaa4e7994885ad0854c47c44e84
DevByDipto <dipto.1900@gmail.com>
2025-03-01 20:45:28 +0600

    first commit

---
+++ script/tailwind.config.js
@@ -0,0 +1,13 @@
+  /** @type {import('tailwindcss').Config} */
+  export default {
+       content: ["./**/*.{html,js}"],
+        theme: {
+          extend: {
+            colors: {
+                '#00303C': '#00303C',
+                'secondary': '#F4F7FF',
+              },
+          },
+        },
+        plugins: [],
+      }
